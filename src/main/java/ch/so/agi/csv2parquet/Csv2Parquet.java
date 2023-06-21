6688bb14a9759ab5f5ec8d103d74388f375a819f
Stefan Ziegler <edi.gonzales@gmail.com>
2023-06-21 17:59:23 +0200

    start with writer

--- src/main/java/ch/so/agi/csv2parquet/Csv2Parquet.java
+++ src/main/java/ch/so/agi/csv2parquet/Csv2Parquet.java
@@ -1,6 +1,9 @@
 package ch.so.agi.csv2parquet;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FilenameUtils;
 
 import ch.ehi.basics.logging.EhiLogger;
 import ch.ehi.basics.logging.StdListener;
@@ -9,14 +12,23 @@ import ch.interlis.iom.IomObject;
 import ch.interlis.iom_j.csv.CsvReader;
 import ch.interlis.iox.IoxEvent;
 import ch.interlis.iox.IoxException;
+import ch.interlis.iox_j.EndBasketEvent;
+import ch.interlis.iox_j.EndTransferEvent;
 import ch.interlis.iox_j.ObjectEvent;
 import ch.interlis.iox_j.StartTransferEvent;
+import ch.interlis.ioxwkf.parquet.ParquetWriter;
 
 public class Csv2Parquet {
     
     public boolean run(Path csvPath, Path outputPath, Settings config) throws IoxException {
         
         System.out.println(config);
+        String csvBaseName = FilenameUtils.getBaseName(csvPath.getFileName().toString());
+        
+        // Weil erstes Element null-Werte hat, fehlen Attribute.
+        // was liefert CSV-Reader genau?
+        // Bekommt man irgendwie "Schema"? Falls ja: String-only Schema für Writer.
+        ParquetWriter writer = new ParquetWriter(Paths.get(outputPath.toString(), csvBaseName + ".parquet").toFile());
         
         CsvReader reader = new CsvReader(csvPath.toFile());
         
@@ -38,46 +50,35 @@ public class Csv2Parquet {
             EhiLogger.traceState("valueSeparator <"+valueSeparator+">.");
         }
 
-        
         IoxEvent event = reader.read();
         while (event instanceof IoxEvent) {
             event = reader.read();
             if (event instanceof ObjectEvent) {
-                
-//                EhiLogger.logState("BBBBBBBBBB");
-//                EhiLogger.logError("CCCCCCCCC", 1);
-//                EhiLogger.logError("DDDDDDDDD", 2);
-//                EhiLogger.getInstance().traceState("AAAAAAAAAAAA");
-//                EhiLogger.traceState("Trace Trace Trace");
-
-//                System.out.println(EhiLogger.getInstance().getTraceFilter());
-
-//                for (String attr : reader.getAttributes()) {
-//                    System.out.println(attr);
-//                }
-                
+                                
                 ObjectEvent iomObjEvent = (ObjectEvent) event;
                 IomObject iomObj = iomObjEvent.getIomObject();
+                
+                writer.write(event);
+
     
 //                System.out.println(iomObj);
 //                System.out.println("ölpreis:"+iomObj.getattrvalue("durchschnittlicher_oelpreis_pro_1000_liter"));
             }
         }
 
-//        writer.write(new EndBasketEvent());
-//        writer.write(new EndTransferEvent());
+        writer.write(new EndBasketEvent());
+        writer.write(new EndTransferEvent());
 
-//        if (writer != null) {
-//            writer.close();
-//            writer = null;
-//        }
+        if (writer != null) {
+            writer.close();
+            writer = null;
+        }
+        
         if (reader != null) {
             reader.close();
             reader = null;
         }
 
-        
-        
         return false;
     }
 }
